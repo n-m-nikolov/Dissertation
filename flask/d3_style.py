3e832503ce20bd650acef67b1df93366d7ff134b
Nikola Nikolov <nikola_m_nikolov@yahoo.co.uk>
2015-10-12 19:13:21 +0100

    random flask tutorial files

--- flask/d3_style.py
+++ flask/d3_style.py
@@ -1,14 +1,25 @@
-from flask import Flask
+from flask import Flask, views
 import flask
 
 app = Flask(__name__)
+app.debug = True
 
 
-@app.route('/index')
-def hello_world():
-    return flask.render_template('index.html')
+class View(flask.views.MethodView) :
+    def get(self):
+        return flask.render_template('index.html')
+
+class Random(flask.views.MethodView):
+    def get(self):
+        return flask.render_template('random.html')
+
+
+app.add_url_rule('/', view_func=View.as_view('main'))
+app.add_url_rule('/random', view_func=Random.as_view('random'))
+# @app.route('/index')
+# def hello_world():
+#     return flask.render_template('index.html')
 
 
 
-if __name__ == '__main__':
-    app.run()
+app.run()
